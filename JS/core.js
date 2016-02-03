ee79386a2f775dc7c9649f11038f96c386db8388
Rictus <contact.dylanleroux@gmail.com>
2016-02-03 18:30:53 +0100

    IdxCoincidence Fix file fo constants

--- JS/core.js
+++ JS/core.js
@@ -1,29 +1,5 @@
 'use strict';
 
-var indexOfCoincidence = [
-    ['Russe', 0.0529],
-    ['Serbe', 0.0643],
-    ['Suedois', 0.0644],
-    ['Anglais', 0.0667],
-    ['Esperanto', 0.0690],
-    ['Grec', 0.0691],
-    ['Norvégien', 0.0694],
-    ['Danois', 0.0707],
-    ['Finnois', 0.0737],
-    ['Italien', 0.0738],
-    ['Portugais', 0.0745],
-    ['Arabe', 0.0758],
-    ['Allemand', 0.0762],
-    ['Hebreu', 0.0768],
-    ['Espagnol', 0.0770],
-    ['Japonais', 0.0772],
-    ['Français', 0.0778],
-    ['Neerlandais', 0.0798],
-    ['Malaysien', 0.0852]
-];
-var NB_LETTERS_ALPHABET = 26;
-var FIRST_LETTER_UPPERCASE_ASCII_INDEX = 65;
-var LAST_LETTER_UPPERCASE_ASCII_INDEX = 65 + NB_LETTERS_ALPHABET - 1;
 var core = {
     process: function (text, onDone) {
         console.group("Texte : ");
@@ -39,7 +15,7 @@ var core = {
     },
     encrypt: {
         vigenere: function (str, key) {
-            console.group("Vigenere encryption");
+            console.groupCollapsed("Vigenere encryption");
             console.time("vigenere_encryption");
             if (typeof key === "undefined" || key.length == 0) {
                 key = "MUSIQUE";
@@ -134,7 +110,7 @@ var core = {
              * @param str
              * @returns {{}}
              */
-            console.group("Char frequency");
+            console.groupCollapsed("Char frequency");
             console.time("char_frequency");
             str = str.toUpperCase();
             var strLen = str.length;
@@ -175,22 +151,22 @@ var core = {
             console.groupEnd();
             return char_frequency;
         },
-        getIndexOfCoincidence: function (input) {
+        getIndexOfCoincidence: function (input, searchClosestLanguage) {
             console.group("Index of coincidence");
             console.time("index_coincidence");
             var curIdxOfCoincidence = 0;
             var nbTotalLetters = 0;
+            searchClosestLanguage = typeof searchClosestLanguage === "boolean" ? searchClosestLanguage : true;
 
             var distance = Number.MAX_VALUE;
             var closestIdxOfCoincidence;
 
             if (typeof input === "object") {
-                console.log(input);
                 var freqObj = input;
             } else if (typeof input === "string") {
                 var char_frequency = [];
                 //from a to z
-                for (var i = FIRST_LETTER_UPPERCASE_ASCII_INDEX; i < LAST_LETTER_UPPERCASE_ASCII_INDEX; i++) {
+                for (var i = FIRST_LETTER_UPPERCASE_ASCII_INDEX; i <= LAST_LETTER_UPPERCASE_ASCII_INDEX; i++) {
                     var ch = String.fromCharCode(i);
                     char_frequency[ch] = {count: 0};
                 }
@@ -208,37 +184,33 @@ var core = {
                 freqObj = char_frequency;
             }
 
-
             for (var key in freqObj) {
                 if (freqObj.hasOwnProperty(key)) {
                     nbTotalLetters += freqObj[key].count;
                 }
             }
-
-            //TODO Ne faire que les lettres qui apparaissent ds le txt
-            console.group("debug");
-            console.log(curIdxOfCoincidence);
             for (key in freqObj) {
-                console.log(input);
-                if (freqObj.hasOwnProperty(key) && freqObj[key].count >= 1) {
-                    console.log((freqObj[key].count * (freqObj[key].count - 1)));
+                if (freqObj.hasOwnProperty(key) && freqObj[key].count > 0) {
                     curIdxOfCoincidence += (freqObj[key].count * (freqObj[key].count - 1)) / (nbTotalLetters * (nbTotalLetters - 1));
                 }
             }
-            console.log(curIdxOfCoincidence);
-            console.groupEnd();
-            console.log(freqObj);
-            for (i = 0; i < indexOfCoincidence.length; i++) {
-                var curDist = Math.abs(indexOfCoincidence[i][1] - curIdxOfCoincidence);
-                if (curDist < distance) {
-                    closestIdxOfCoincidence = i;
-                    distance = curDist;
+            var returnObj;
+            if (searchClosestLanguage) {
+                for (i = 0; i < languagesIdxOfCoincidence.length; i++) {
+                    var curDist = Math.abs(languagesIdxOfCoincidence[i][1] - curIdxOfCoincidence);
+                    if (curDist < distance) {
+                        closestIdxOfCoincidence = i;
+                        distance = curDist;
+                    }
                 }
-            }
+                returnObj = [languagesIdxOfCoincidence[closestIdxOfCoincidence][0], curIdxOfCoincidence];
 
+            } else {
+                returnObj = curIdxOfCoincidence;
+            }
             console.timeEnd("index_coincidence");
             console.groupEnd();
-            return [indexOfCoincidence[closestIdxOfCoincidence][0], curIdxOfCoincidence];
+            return returnObj;
         },
         vigenere: function (cryptedText) {
             var charF = this.getCharFrequency(cryptedText);
@@ -246,13 +218,19 @@ var core = {
 
             console.group("Vigenere Analyze");
             console.time("vigenere_analyze");
-            for (var nbLet = 1; nbLet < 10; nbLet++) {
+            for (var nbLet = 2; nbLet < 10; nbLet++) {
                 var pattern = "([a-zA-Z0-9]{1," + nbLet + "})";
                 var regex = new RegExp(pattern, "g");
                 var matches = cryptedText.match(regex);
                 //calculate IC for each matches
-                for (var matchIdx = 0; matchIdx < matches.length; matchIdx++) {
-                    console.log(this.getIndexOfCoincidence(matches[matchIdx]));
+                for (var matchIdx = 0; matchIdx < matches.length - 1; matchIdx++) {
+                    var idx = this.getIndexOfCoincidence(matches[matchIdx], false);
+                    //TODO
+                    if(idx == 0) { //Ignorer
+                        console.warn(matches[matchIdx]);
+                    } else {
+                        //Put in array
+                    }
                 }
             }
             console.timeEnd("vigenere_analyze");
